cf70694b6611c8e39aaffbdded1d2ad4630c2b43
div50015 <div50015@mail.ru>
2025-04-30 09:49:19 +0300

    edit 9

--- tmp.py
+++ tmp.py
@@ -1,16 +1,17 @@
-import time
+import nmap
 
-def nod(a, b):
-    while a != b:
-        if a > b:
-            a -= b
-        else:
-            b -= a
-    return a
 
-st = time.time()
-c = nod(10000000,1)
-ed = time.time()
-tt = st - ed
-print(c)
-print(tt)
+nm = nmap.PortScanner()
+# nm.scan(hosts="10.30.92.0/24", arguments='-min-rtt-timeout 100ms -sP')
+#
+# for host in nm.all_hosts():
+#     print(host)
+
+
+try:
+    nm.scan(hosts='10.30.92.0/24', arguments='-sP')
+    [print(host) for host in nm.all_hosts()]
+    print("********************\r\n")
+except Exception as e:
+    print(type(e))
+
